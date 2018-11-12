fd0e43bfb5e133468a15799ff6e6fb32b8ad6130
Derek Dapp <derekdapp@gmail.com>
2018-11-12 14:11:36 -0700

    initial commit

---
+++ casino.rb
@@ -0,0 +1,7 @@
+# -Start game player has a name and an initial bankroll
+# -Player can go to different games via menu
+#    Slots
+#    High / Low
+# -Player places bet and wins / loses
+# -Player's bankroll goes up and down with 
+# wins and losses
